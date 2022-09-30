c88edc307becea408ac044b809664ea229049c7b
Kenny <kennysoyoye07@gmail.com>
2022-09-30 10:32:01 +0100

    using middlewares

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const path = require("path");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// the whilelist is an array of domains that can access the server
+const whitelist = ["https://www.site.com", "http://localhost:3500"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    // remove !== origin after develpoment
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data in other words,form data
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+// this serves all the files in the public folder (css, images and more...)
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
@@ -45,8 +76,18 @@ const three = (req, res) => {
 app.get("/chain(.html)?", [one, two, three]);
 
 // if it doesn't find the route specified
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("/*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
+
+// error handling
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
