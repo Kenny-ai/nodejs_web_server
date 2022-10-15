12d04bead71736088f5af4af1a9bcf2ee06ead0f
Kenny <kennysoyoye07@gmail.com>
2022-10-15 20:45:44 +0100

    refactored allowed origins to an independent module

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,10 +1,10 @@
 // the whilelist is an array of domains that can access the server
+const { allowedOrigins } = require("./allowedOrigins");
 
-const whitelist = ["https://www.site.com", "http://localhost:3500"];
 exports.corsOptions = {
   origin: (origin, callback) => {
     // remove !== origin after develpoment
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
